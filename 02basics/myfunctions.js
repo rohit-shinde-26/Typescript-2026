28edb766a2a89b34147efd06428e2fa1c60b5302
Rohit-shinde <100394511+rohit-shinde-26@users.noreply.github.com>
2026-03-17 11:25:10 +0530

    Add myfunctions.js and myfunctions.ts with utility functions for number and string manipulation

---
+++ 02basics/myfunctions.js
@@ -0,0 +1,19 @@
+"use strict";
+exports.__esModule = true;
+function addTwo(num) {
+  // return num + 2
+  return "hello";
+}
+function getUpper(val) {
+  return val.toUpperCase();
+}
+function signUpUser(name, email, isPaid) {}
+var loginUser = function (name, email, isPaid) {
+  if (isPaid === void 0) {
+    isPaid = false;
+  }
+};
+var myValue = addTwo(5);
+getUpper("Ram");
+signUpUser("Ram", "ram@lco.dev", false);
+loginUser("Ram", "ram@h.com");
