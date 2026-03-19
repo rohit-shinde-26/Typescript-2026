4386fb5525e61cf8bf256b7f26509fc5090604da
Rohit-shinde <100394511+rohit-shinde-26@users.noreply.github.com>
2026-03-19 13:59:53 +0530

    Add myObjects.js and myObjects.ts with User type definition and example implementation

---
+++ 02basics/myObjects.js
@@ -0,0 +1,15 @@
+"use strict";
+// const User = {
+//     name: "ram",
+//     email: "ram@lco.dev",
+//     isAvtive: true
+// }
+exports.__esModule = true;
+var myUser = {
+  _id: "1245",
+  name: "ram",
+  email: "ram@lco.dev",
+  isActive: true,
+};
+myUser.email = "h@gmail.com";
+myUser._id = "asa";
